d1a68a7ddfda35e44736d1f72f0da82a330208f7
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-07-13 14:28:35 +0900

    bugsnag 導入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import Bugsnag from '@bugsnag/js';
+import { BugsnagErrorHandler } from '@bugsnag/plugin-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+Bugsnag.start({ apiKey: '0100aa5f85562c34a44a44a571014648' });
+
+export function errorHandlerFactory() {
+  return new BugsnagErrorHandler();
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [{ provide: ErrorHandler, useFactory: errorHandlerFactory }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
